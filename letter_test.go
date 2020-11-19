38e75b211b9362127c0250ac877a453d65d6aab5
kate smith <kateesmith4994@gmail.com>
2020-11-19 16:59:33 +0000

    refactored even test and passing another acceptance test

--- letter_test.go
+++ letter_test.go
@@ -12,14 +12,21 @@ func TestReturnsMiddleLetter(t *testing.T) {
 			t.Errorf("got %s, wanted %s", got, want)
 		}
 	})
+}
+func TestEvenValueLetters(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		// acceptance criteria tests
+		{input: "test", want: "es"},
+		{input: "middle", want: "dd"},
+	}
 
-	t.Run("Even value letters", func(t *testing.T) {
-		got := getMiddle("test")
-		want := "es"
-
-		if got != want {
-			t.Errorf("got %s, wanted %s", got, want)
+	for _, tc := range tests {
+		got := getMiddle(tc.input)
+		if tc.want != got {
+			t.Errorf("expected: %s, got: %s", tc.want, got)
 		}
-	})
-
+	}
 }
