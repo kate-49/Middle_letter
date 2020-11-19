d7892c8bb0ddd175f801fca021b6757a542531e0
kate smith <kateesmith4994@gmail.com>
2020-11-19 16:56:22 +0000

    passing test for even letters

--- letter.go
+++ letter.go
@@ -5,7 +5,14 @@ func getMiddle(word string) string {
 
 	stringLength := len(word)
 	if stringLength%2 == 0 {
-
+		for index, letter := range word {
+			if index == (stringLength/2 - 1) {
+				answer += string(letter)
+			}
+			if index == (stringLength / 2) {
+				answer += string(letter)
+			}
+		}
 	} else {
 		for index, letter := range word {
 			if index == stringLength/2 {
