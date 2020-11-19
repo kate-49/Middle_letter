1a18710357b2608d6a45bdbbd8bda665ab7b1dae
kate smith <kateesmith4994@gmail.com>
2020-11-19 16:54:28 +0000

    refactored odd string test

--- letter_test.go
+++ letter_test.go
@@ -3,10 +3,14 @@ package letter
 import "testing"
 
 func TestReturnsMiddleLetter(t *testing.T) {
-	got := getMiddle("testing")
-	want := "t"
 
-	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
-	}
+	t.Run("Odd value letters", func(t *testing.T) {
+		got := getMiddle("testing")
+		want := "t"
+
+		if got != want {
+			t.Errorf("got %s, wanted %s", got, want)
+		}
+	})
+
 }
